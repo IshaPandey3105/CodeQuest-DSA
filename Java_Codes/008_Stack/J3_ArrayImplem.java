a31ce9ea2193923c74fea7898c42bcf86953bdf4
IshaPandey3105 <pandeyisha3105@gmail.com>
2025-11-21 16:47:01 +0530

    Added dual stack implementations (Array + Linked List) for better understanding and comparison

---
+++ Java_Codes/008_Stack/J3_ArrayImplem.java
@@ -0,0 +1,75 @@
+ class stack{
+    private int[] arr=new int[5];
+    private int indx=0;
+
+    void push(int elem){
+        if(isFull()){
+            System.out.println("OverFlow");
+            return;
+        }
+        arr[indx]=elem;
+        indx++;
+    }
+
+    int peek(){
+        if(isFull()){
+            System.out.println("OverFlow");
+            return -1;
+        }
+        return arr[indx-1];
+    }
+
+    int pop(){
+        if(isEmpty()){
+            System.out.println("UnderFlow");
+            return -1;
+        }
+        int elem=arr[indx-1];
+        arr[indx-1]=0;
+        indx--;
+        return elem;
+    }
+
+    int size(){
+        return indx;
+    }
+
+    boolean isFull(){
+        if(indx==arr.length) return true;
+        return false;
+    }
+
+    boolean isEmpty(){
+        if(indx==0) return true;
+        return false;
+    }
+
+    void Display(){
+        for(int i=0;i<=indx-1;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+public class J3_ArrayImplem {
+    public static void main(String[] args) {
+        stack st=new stack();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        System.out.println(st.peek());
+        System.out.println(st.pop());
+        System.out.println(st.size());
+        st.push(4);
+        st.push(5);
+        st.Display();
+        System.out.println(st.isFull());
+        System.out.println(st.isEmpty());
+        System.out.println(st.pop());
+        System.out.println(st.pop());
+        System.out.println(st.pop());
+        System.out.println(st.pop());
+        System.out.println(st.pop());
+        System.out.println(st.isEmpty());
+    }
+}
---
+++ Java_Codes/008_Stack/J4_LLImplem.java
@@ -0,0 +1,74 @@
+class Node{
+    int val;
+    Node next;
+    Node(int val){
+        this.val=val;
+        this.next=null;
+    }
+}
+class Stack{
+    private Node head=null;
+    private int size=0;
+
+    void push(int elem) {
+        Node newNode=new Node(elem);
+        newNode.next=head;
+        head=newNode;
+        size++;
+    }
+
+    int pop(){
+        if(head==null){
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        int top=head.val;
+        head=head.next;
+        size--;
+        return top;
+    }
+
+    int peek(){
+        if(head==null) return -1;
+        return head.val;
+    }
+
+    int size(){
+        return size;
+    }
+
+    boolean isEmpty(){
+        if(head==null) return true;
+        return false;
+    }
+
+    void Display(){
+        // For reverse order use loop traversing
+        // Node temp=head;
+        // while(temp!=null){
+        //     System.out.print(temp.val +" ");
+        //     temp=temp.next;
+        // }
+        // For same order use recursion
+        newDisplay(head);
+    }
+    void newDisplay(Node head){
+        if(head==null) return;
+        newDisplay(head.next);
+        System.out.print(head.val +" ");
+    }
+}
+public class J4_LLImplem {
+    public static void main(String[] args) {
+        Stack st=new Stack();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        System.out.println(st.pop()); 
+        st.Display();   
+        System.out.print("\npeek elem : "+st.peek());   
+        System.out.println("\n"+st.isEmpty());   
+        System.out.println("Size is : "+st.size());   
+    }
+}
