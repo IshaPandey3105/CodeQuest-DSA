07dfdf42fdd3b2ba6b0c79b0389973b0e909c855
IshaPandey3105 <pandeyisha3105@gmail.com>
2025-11-25 13:03:28 +0530

    Practiced Important Questions like Next greatest and stock span problem

---
+++ Java_Codes/008_Stack/J5_NextGreatestElem.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+public class J5_NextGreatestElem {
+
+    public static void Print(int[] arr){
+        for(int elem : arr){
+            System.out.print(elem + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr={1,3,2,4};
+        // int[] arr={2,4,3,5,1};
+        int[] ans=new int[arr.length];
+        Stack<Integer> st=new Stack<>();
+        int n=arr.length;
+        // int i=n-1;
+        // while(i>=0){
+        //     if(st.isEmpty()) {
+        //         ans[i]=-1;
+        //     }
+        //     else if(st.peek()>arr[i]) {
+        //         ans[i]=st.peek();
+        //     }else{
+        //         while(!st.isEmpty() && arr[i]>=st.peek()){
+        //             st.pop();
+        //         }
+        //         if(st.isEmpty()) ans[i]=-1;
+        //         else ans[i]=st.peek();
+        //     }
+        //     i--;
+        //     st.push(arr[i]);
+        // }
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && arr[i]>=st.peek()){
+                    st.pop();
+            }
+            ans[i]=st.isEmpty()?-1:st.peek();
+            st.push(arr[i]);
+        }
+        Print(arr);
+        Print(ans);
+    }
+}
---
+++ Java_Codes/008_Stack/J6_StockSpan.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+public class J6_StockSpan {
+    public static void Print(int[] arr){
+        for(int elem : arr){
+            System.out.print(elem + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr={100,80,60,70,60,75,85,200};
+        int[] ans=new int[arr.length];
+        Stack<Integer> st=new Stack<>();
+        st.push(0);
+        int n=arr.length;
+        for(int i=0;i<n;i++){    // i=days
+            while(!st.isEmpty() && arr[st.peek()]<=arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()) ans[i]=i+1;
+            else ans[i]=i-st.peek();
+            st.push(i);
+        }
+        Print(arr);
+        Print(ans);
+    }
+}
