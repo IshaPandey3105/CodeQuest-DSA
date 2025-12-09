6318dfcafde42614761b9a3c8d464b96e5472555
IshaPandey3105 <pandeyisha3105@gmail.com>
2025-12-09 17:54:50 +0530

    Practiced More Questions of Stack also evaluate infix prefix and postfix expression using stack

---
+++ Java_Codes/008_Stack/J7_Infix.java
@@ -0,0 +1,73 @@
+import java.util.Stack;
+
+public class J7_Infix {
+    public static void main(String[] args) {
+        String str = "8-(5+3)*4/6";
+        System.out.println("Expression: " + str);
+
+        Stack<Integer> val = new Stack<>();
+        Stack<Character> op = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            // If digit, push to val stack
+            if (ch >= '0' && ch <= '9') {
+                val.push(ch - '0');
+            }
+            // If '(', push to op stack
+            else if (ch == '(') {
+                op.push(ch);
+            }
+            // If ')', solve till '('
+            else if (ch == ')') {
+                while (op.peek() != '(') {
+                    int v2 = val.pop();
+                    int v1 = val.pop();
+                    char opr = op.pop();
+
+                    if (opr == '+') val.push(v1 + v2);
+                    else if (opr == '-') val.push(v1 - v2);
+                    else if (opr == '*') val.push(v1 * v2);
+                    else if (opr == '/') val.push(v1 / v2);
+                }
+                op.pop(); // pop '('
+            }
+            // If operator
+            else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+                while (!op.isEmpty() && precedence(op.peek()) >= precedence(ch)) {
+                    int v2 = val.pop();
+                    int v1 = val.pop();
+                    char opr = op.pop();
+
+                    if (opr == '+') val.push(v1 + v2);
+                    else if (opr == '-') val.push(v1 - v2);
+                    else if (opr == '*') val.push(v1 * v2);
+                    else if (opr == '/') val.push(v1 / v2);
+                }
+                op.push(ch);
+            }
+        }
+
+        // Solve remaining operators
+        while (!op.isEmpty()) {
+            int v2 = val.pop();
+            int v1 = val.pop();
+            char opr = op.pop();
+
+            if (opr == '+') val.push(v1 + v2);
+            else if (opr == '-') val.push(v1 - v2);
+            else if (opr == '*') val.push(v1 * v2);
+            else if (opr == '/') val.push(v1 / v2);
+        }
+
+        System.out.println("Result: " + val.pop());
+    }
+
+    // Define operator precedence
+    public static int precedence(char ch) {
+        if (ch == '+' || ch == '-') return 1;
+        if (ch == '*' || ch == '/') return 2;
+        return 0;
+    }
+}
---
+++ Java_Codes/008_Stack/J9_InfixToPostfix.java
@@ -0,0 +1,74 @@
+import java.util.Stack;
+
+public class J9_InfixToPostfix {
+    public static void main(String[] args) {
+        String str = "8-(5+3)*4/6";
+        System.out.println("Expression: " + str);
+
+        Stack<String> val = new Stack<>();
+        Stack<Character> op = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            int ascii = (int)ch;
+
+            // number
+            if (ascii >= 48 && ascii <= 57) {
+                val.push("" + ch);
+            }
+            // opening bracket
+            else if (ch == '(') {
+                op.push(ch);
+            }
+            // closing bracket
+            else if (ch == ')') {
+                while (op.peek() != '(') {
+                    String v2 = val.pop();
+                    String v1 = val.pop();
+                    char opr = op.pop();
+                    String t =   v1 + v2 + opr;
+                    val.push(t);
+                }
+                op.pop(); // pop '('
+            }
+            // operator
+            else {
+                // for + and -
+                if (ch == '+' || ch == '-') {
+                    // solve all previous ops (except '(') becuz we can push them when '(' at top
+                    while (!op.isEmpty() && op.peek() != '(') {
+                        String v2 = val.pop();
+                        String v1 = val.pop();
+                        char opr = op.pop();
+                        String t = v1 + v2 + opr;
+                        val.push(t);
+                    }
+                    op.push(ch);
+                }
+                // for * and /
+                else if (ch == '*' || ch == '/') {
+                    // solve only if previous has same precedence (* or /)
+                    while (!op.isEmpty() && (op.peek() == '*' || op.peek() == '/')) {
+                        String v2 = val.pop();
+                        String v1 = val.pop();
+                        char opr = op.pop();
+                        String t = v1 + v2 + opr;
+                        val.push(t);
+                    }
+                    op.push(ch);
+                }
+            }
+        }
+
+        // solve remaining operators
+        while (!op.isEmpty()) {
+            String v2 = val.pop();
+            String v1 = val.pop();
+            char opr = op.pop();
+            String t = v1 + v2 + opr;
+            val.push(t);
+        }
+
+        System.out.println("Postfix: " + val.peek());
+    }
+}
